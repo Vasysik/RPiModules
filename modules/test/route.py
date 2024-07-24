2802952e1a396ff92963006103df4f31042af2a3
Amadeus <vasiavasia10000@gmail.com>
2024-07-24 20:38:54 +0300

    Bugs Fix

--- modules/test/route.py
+++ modules/test/route.py
@@ -2,11 +2,11 @@ from flask import Blueprint, render_template
 import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-fan_control = Blueprint('test', __name__,
+test = Blueprint('test', __name__,
                         template_folder='templates',
                         static_folder='static',
                         static_url_path='static')
 
-@fan_control.route('/')
+@test.route('/')
 def index():
     return render_template('test.html')
