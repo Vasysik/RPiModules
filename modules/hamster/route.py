3ed48df0715802cf8fc331b949708c2175a49c80
Amadeus <vasiavasia10000@gmail.com>
2024-07-26 16:39:58 +0300

    Added /hamster/api/current/<user>/<element>

--- modules/hamster/route.py
+++ modules/hamster/route.py
@@ -19,6 +19,17 @@ def index():
 def api_current():
     if request.method == 'GET':
         return jsonify(read_json(os.path.join(current_dir, 'current.json')))
+    
+@hamster.route('/api/current/<user>/<element>', methods=['GET'])
+def api_current_user_element(user, element):
+    data = read_json(os.path.join(current_dir, 'current.json'))
+    user_data = data.get(user, {})
+    element_value = user_data.get(element, None)
+
+    if element_value is None:
+        return jsonify({"error": "Element not found"}), 404
+
+    return jsonify({element: element_value})
 
 @hamster.route('/api/status', methods=['GET'])
 def api_status():
