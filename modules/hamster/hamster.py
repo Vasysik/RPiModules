82023fbd34914b1a632aa4c96ddad5d32979d0be
Amadeus <vasiavasia10000@gmail.com>
2024-07-26 16:21:45 +0300

    Bugs fix

--- modules/hamster/hamster.py
+++ modules/hamster/hamster.py
@@ -1,5 +1,4 @@
-import pexpect
-import time, json, sys
+import pexpect, time, json, sys, re
 
 def update_status(status, end_string=""):
     with open('status.json', 'w') as f:
@@ -8,6 +7,10 @@ def update_status(status, end_string=""):
 def read_config():
     with open('config.json', 'r') as f:
         return json.load(f)
+    
+def remove_ansi_sequences(text):
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    return ansi_escape.sub('', text)
 
 def run_script():
     update_status("running")
@@ -37,7 +40,7 @@ def run_script():
     while True:
         try:
             process.expect(pexpect.TIMEOUT, timeout=1)
-            last_line = process.before.splitlines()[-1]
+            last_line = remove_ansi_sequences(process.before).splitlines()[-1]
             update_status("running", last_line)
         except pexpect.TIMEOUT:
             continue
