9c8debe8c50ac17cab25bb960f281cac3a4287f4
Amadeus <vasiavasia10000@gmail.com>
2024-07-27 01:18:52 +0300

    hamsterkombat update

--- modules/hamster/route.py
+++ modules/hamster/route.py
@@ -11,6 +11,13 @@ def read_json(file_path):
     with open(file_path, 'r') as f:
         return json.load(f)
 
+config = read_json(os.path.join(current_dir, 'hamsterkombat/config.json'))
+    
+from influxdb_client import InfluxDBClient, QueryApi
+client = InfluxDBClient(url=read_json(config['influxdb_config_path'])['influxdb_url'], 
+                        token=read_json(config['influxdb_config_path'])['influxdb_token'])
+query_api = client.query_api()
+
 @hamster.route('/')
 def index():
     return render_template('hamster.html')
@@ -36,7 +43,7 @@ def api_status():
 
 @hamster.route('/api/config', methods=['GET'])
 def api_config():
-    return jsonify(read_json(os.path.join(current_dir, 'config.json')))
+    return jsonify(read_json(os.path.join(current_dir, 'hamster_config.json')))
 
 @hamster.route('/api/tokens', methods=['GET', 'POST', 'DELETE'])
 def api_tokens():
@@ -66,3 +73,41 @@ def api_tokens():
                 f.writelines([line for line in tokens if line.strip() != token])
             return jsonify({"message": "Token removed successfully"}), 200
         return jsonify({"error": "Invalid token"}), 400
+    
+@hamster.route('/api/graph_data')
+def get_graph_data():
+    graph_type = request.args.get('type', 'earnPassivePerHour')
+    time_range = int(request.args.get('range', 86400))  # Default to 1 minute
+
+    # Определяем интервал агрегации в зависимости от временного диапазона
+    if time_range <= 86400:  # 1 день или меньше
+        interval = '1h'
+    elif time_range <= 604800:  # 7 дней или меньше
+        interval = '6h'
+    elif time_range <= 2592000:  # 1 месяц или меньше
+        interval = '1d'
+    else:
+        interval = '1w'
+
+    query = f"""
+    from(bucket:"{config['influxdb_bucket']}")
+    |> range(start: -{time_range}s)
+    |> filter(fn: (r) => r._measurement == "measurement")
+    |> filter(fn: (r) => r._field == "{graph_type}")
+    |> aggregateWindow(every: {interval}, fn: mean, createEmpty: false)
+    |> yield(name: "mean")
+    """
+
+    result = client.query_api().query(query, org=config['influxdb_org'])
+
+    timestamps = []
+    values = []
+    fan_states = []
+
+    for table in result:
+        for record in table.records:
+            timestamps.append(record.get_time().strftime('%Y-%m-%d %H:%M:%S'))
+            if record.get_field() == graph_type:
+                values.append(record.get_value())
+            elif record.get_field() == 'fan_state':
+                fan_states.append(round(record.get_value()))  # Округляем состояние вентилятора
